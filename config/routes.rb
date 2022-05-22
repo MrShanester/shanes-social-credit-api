8ab45698bca1c6eb5ae99d91ec5bc76d6180f5d9
Shane Nicholson <shanescottnicholson@gmail.com>
2022-05-22 16:11:16 -0700

    Adds routes for the deeds controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # deeds routes
+  get "/deed" => "deeds#index"
+  get "/deed/:id" => "deeds#show"
+  post "/deed" => "deeds#create"
+  patch "/deed/:id" => "deeds#update"
+  delete "/deed/:id" => "deeds#destroy"
 end
